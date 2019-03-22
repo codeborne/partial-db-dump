6824d6dfdde02ef6452600e0fd72798b1ef337c8
Anton Keks <anton@codeborne.com>
2019-03-22 16:37:49 +0200

    close result sets

--- src/JdbcHelpers.kt
+++ src/JdbcHelpers.kt
@@ -1,7 +1,10 @@
 import java.sql.ResultSet
 
-fun <T: Any> ResultSet.readAll(mapper: (ResultSet) -> T) = generateSequence {
+fun <T : Any> ResultSet.readAll(mapper: (ResultSet) -> T) = use {
+  generateSequence {
     if (next()) mapper(this) else null
+  }.toList()
 }
 
 operator fun ResultSet.get(key: String) = getString(key)
+operator fun ResultSet.get(n: Int) = getObject(n)
