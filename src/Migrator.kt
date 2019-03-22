25bd68a7da8ca84e52939d2782ca88d72a134307
Anton Keks <anton@codeborne.com>
2019-03-22 11:39:20 +0200

    list all tables with deps

--- src/Migrator.kt
+++ src/Migrator.kt
@@ -7,25 +7,24 @@ class Migrator(private val dbUrl: String) {
       val metaData = conn.metaData
       val tables = listTables(metaData)
       val foreignKeys = listForeignKeys(metaData)
-      val tableDependencies = foreignKeys.groupBy { it.pkTable }
 
-      println(tableDependencies.entries.joinToString("\n"))
+      foreignKeys.forEach {
+        tables[it.pkTable]!!.deps += tables[it.fkTable]!!
+      }
 
-      val tablesNoDeps = tables.filter { !tableDependencies.containsKey(it) }.toList()
-      println(tablesNoDeps.size)
+      println(tables.values.joinToString("\n"))
     }
   }
 
   private fun listTables(metaData: DatabaseMetaData) =
     metaData.getTables(null, metaData.userName, null, arrayOf("TABLE")).readAll {
-      it["TABLE_NAME"]
-    }
+      Table(it["TABLE_NAME"])
+    }.associateBy { it.name }
 
   private fun listForeignKeys(metaData: DatabaseMetaData) =
     metaData.getImportedKeys(null, metaData.userName, null).readAll {
       ForeignKey(it["FKTABLE_NAME"], it["FKCOLUMN_NAME"], it["PKTABLE_NAME"], it["PKCOLUMN_NAME"])
     }
-
 }
 
 data class ForeignKey(
@@ -35,4 +34,10 @@ data class ForeignKey(
   val pkColumn: String
 ) {
   override fun toString() = "$fkTable.$fkColumn -> $pkTable.$pkColumn"
+}
+
+data class Table(val name: String) {
+  val deps = mutableListOf<Table>()
+
+  override fun toString() = "$name <- ${deps.joinToString { it.name }}"
 }
